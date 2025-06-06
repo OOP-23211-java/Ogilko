de8f4970de79fa1f818d787e1cec40fd8e3f7b87
Дима <115703951+Gispoo@users.noreply.github.com>
2025-06-06 08:13:47 +0700

    добавил объект спициалист и пользователь

---
+++ tasks/BookingSpecialist/src/main/java/com/NSU/BookingSpecialist/model/Specialist.java
@@ -0,0 +1,25 @@
+package com.NSU.BookingSpecialist.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Specialist {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String profession;
+}
